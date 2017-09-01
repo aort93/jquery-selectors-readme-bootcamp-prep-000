1fbe6aca3090eeadd62933df0ba108bcf8598a0f
Alonso Ortega <alonsojortega@gmail.com>
2017-09-01 07:28:56 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,2 +1,20 @@
 // declare your functions here...
-$('img')
+function paragraphSelector(){
+  return $('p');
+}
+
+function lastImageSelector(){
+  return $('img:last');
+}
+
+function ninjaBabySelector(){
+  return $('#baby-ninja')
+}
+
+function divSelector(){
+  return $('.pics');
+}
+
+function firstListItem(){
+  return $('ul#pic-list li:first-child');
+}
